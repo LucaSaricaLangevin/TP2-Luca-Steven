10257235429c65e968fc082b9f284cc5c1b6dc43
Steven <steven.nanogame@gmail.com>
2025-10-10 13:29:44 -0400

    ajusté le function_view.py

--- function_view.py
+++ function_view.py
@@ -1,5 +1,5 @@
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QVBoxLayout, QLineEdit, QCheckBox, QPushButton
+from PyQt6.QtWidgets import QVBoxLayout, QLineEdit, QCheckBox, QPushButton, QMainWindow
 
 
 class FunctionView(QMainWindow):

4902533820da965a679ff12ccaf42cfff08640b2
Steven Roseberry <89659775+StevenRoseberry@users.noreply.github.com>
2025-10-26 20:55:29 -0400

    Update main_window_view.py

--- views/main_window_view.py
+++ views/main_window_view.py
@@ -1,16 +1,17 @@
 import os
 from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QLineEdit, QSlider, QComboBox, QPushButton, QMessageBox, \
     QGroupBox, QLabel
+from PyQt6.QtCore import Qt
 from PyQt6.uic import loadUi
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
 
 from canvas.matplotlib_canvas import PlotCanvas
 from models.main_window_model import MainWindowModel
-
+from models.function_list_model import FunctionListModel
+from views.function_list_view import FunctionListView
 
 
 class MainWindowView(QMainWindow):
-
     functionLineEdit: QLineEdit
     infLineEdit: QLineEdit
     supLineEdit: QLineEdit
@@ -25,14 +26,31 @@ class MainWindowView(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("../ui/mainWindow.ui", self)
+        
+        # Définir la taille de la fenêtre par défaut
+        self.resize(1400, 900)
+        self.setMinimumSize(1000, 700)
 
+        # Modèle principal
         self.model = MainWindowModel()
+        
+        # Canvas matplotlib
         self.canvas = PlotCanvas(self.model)
         toolbar = NavigationToolbar(self.canvas, self)
         self.functionLayout.insertWidget(0, toolbar)
         self.functionLayout.insertWidget(1, self.canvas)
 
-        # connexions
+        # Créer le modèle et la vue de la liste de fonctions
+        self.function_list_model = FunctionListModel()
+        self.function_list_view = FunctionListView(self.function_list_model)
+        
+        # Ajouter la vue comme DockWidget à droite
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.function_list_view)
+        
+        # Configurer le menu pour afficher/masquer le dock
+        self.setup_menu()
+
+        # Connexions des signaux
         self.functionLineEdit.editingFinished.connect(self.on_function_edited)
         self.infLineEdit.textChanged.connect(self.on_borne_inf_edited)
         self.supLineEdit.textChanged.connect(self.on_borne_sup_edited)
@@ -40,6 +58,12 @@ class MainWindowView(QMainWindow):
         self.nombreSlider.sliderMoved.connect(self.on_nb_rectangles_changed)
         self.orientationComboBox.currentIndexChanged.connect(self.on_orientation_changed)
         self.calculerButton.clicked.connect(self.on_calculer_clicked)
+
+    def setup_menu(self):
+        """Configure le menu pour afficher/masquer le dock"""
+        toggle_action = self.function_list_view.toggleViewAction()
+        toggle_action.setText("Afficher/Masquer liste des fonctions")
+        self.menufonction.addAction(toggle_action)
 
     def on_function_edited(self):
         f_str = self.functionLineEdit.text()

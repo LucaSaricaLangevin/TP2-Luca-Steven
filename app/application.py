6a9ce9ada545a61dde18198a20e27c1dc79890c3
Nanogamexe <stevenjeux123@hotmail.com>
2025-10-27 22:41:09 -0400

    commentaires

--- app/application.py
+++ app/application.py
@@ -5,6 +5,8 @@ from views.main_window_view import MainWindowView
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
+
+    # Les thèmes ont en parti été fait par Claude, le changement de thème a été assisté par Claude.
     # Charger le thème par défaut (dark)
     # Le toggle sera géré par MainWindowView
     with open("../styles/dark_theme.qss", "r", encoding="utf-8") as f:
